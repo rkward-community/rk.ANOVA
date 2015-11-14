81f5e4c5d0cc403612f1e31e22aa146174565e4c
unDocUMeantIt <m@reaktanz.de>
2015-11-15 00:06:21 +0100

    updated generator script and enabled translations
      - the script is now optimized for rkwarddev >= 0.07-4
      - added *.pot file
      - added *.po with german translation

--- inst/rkward/plugins/Interactionplot.js
+++ inst/rkward/plugins/Interactionplot.js
@@ -4,120 +4,118 @@
 
 
 function preprocess(){
-	// add requirements etc. here
-	echo("require(sciplot)\n");
+  // add requirements etc. here
+  echo("require(sciplot)\n");
 }
 
 function calculate(){
 }
 
 function printout(){
-	// all the real work is moved to a custom defined function doPrintout() below
-	// true in this case means: We want all the headers that should be printed in the output:
-	doPrintout(true);
+  // all the real work is moved to a custom defined function doPrintout() below
+  // true in this case means: We want all the headers that should be printed in the output:
+  doPrintout(true);
 }
 
 function preview(){
-	preprocess();
-	calculate();
-	doPrintout(false);
+  preprocess();
+  calculate();
+  doPrintout(false);
 }
 
 function doPrintout(full){
-	// read in variables from dialog
+  // read in variables from dialog
+  var ipFactor = getString("ipFactor");
+  var ipResponse = getString("ipResponse");
+  var ipGroups = getString("ipGroups");
+  var ipPlotType = getString("ipPlotType");
+  var ipPlotElements = getString("ipPlotElements");
+  var ipPlotBars = getString("ipPlotBars");
+  var ipClipping = getString("ipClipping");
+  var ipLegendLabel = getString("ipLegendLabel");
+  var ipUpperError = getBoolean("ipUpperError.state");
+  var ipLowerError = getBoolean("ipLowerError.state");
+  var ipLegendChecked = getBoolean("ipLegend.checked");
+  var ipDrawSEChecked = getBoolean("ipDrawSE.checked");
 
-	var vrslFactrxxs = getString("vrsl_Factrxxs");
-	var vrslRspnsvct = getString("vrsl_Rspnsvct");
-	var vrslGrpngfct = getString("vrsl_Grpngfct");
-	var radPlottype = getString("rad_Plottype");
-	var radElements = getString("rad_Elements");
-	var radBars = getString("rad_Bars");
-	var drpClipping = getString("drp_Clipping");
-	var inpLegndlbl = getString("inp_Legndlbl");
-	var chcUpperrrr = getBoolean("chc_Upperrrr.state");
-	var chcLowerrrr = getBoolean("chc_Lowerrrr.state");
-	var frmLegendChecked = getBoolean("frm_Legend.checked");
-	var frmDrwstndrChecked = getBoolean("frm_Drwstndr.checked");
+  // create the plot
+  if(full) {
+    new Header(i18n("Interaction plot")).print();
+  } else {}
 
-	// create the plot
-	if(full) {
-		new Header(i18n("Interaction plot")).print();
+  // in case there are generic plot options defined:
+  var embRkwrdpltptnGCodePreprocess = getValue("emb_rkwrdpltptnG.code.preprocess");
+  var embRkwrdpltptnGCodePrintout = getValue("emb_rkwrdpltptnG.code.printout");
+  var embRkwrdpltptnGCodeCalculate = getValue("emb_rkwrdpltptnG.code.calculate");
 
-	}
+  if(full) {
+    echo("rk.graph.on()\n");
+  } else {}
+  echo("  try({\n");
 
-	// in case there are generic plot options defined:
-	var embRkwrdpltptnGCodePreprocess = getValue("emb_rkwrdpltptnG.code.preprocess");
-	var embRkwrdpltptnGCodePrintout = getValue("emb_rkwrdpltptnG.code.printout");
-	var embRkwrdpltptnGCodeCalculate = getValue("emb_rkwrdpltptnG.code.calculate");
+  // insert any option-setting code that should be run before the actual plotting commands:
+  printIndentedUnlessEmpty("    ", embRkwrdpltptnGCodePreprocess, "\n", "");
 
-	if(full) {
-		echo("rk.graph.on()\n");
-	}
-	echo("\ttry({\n");
+  // the actual plot:
+  var ipLegendChecked = getValue("ipLegend.checked");
+  var ipDrawSEChecked = getValue("ipDrawSE.checked");
+  if(ipPlotType == "line") {
+    echo("\t\tlineplot.CI(");  
+  } else {
+    echo("\t\tbargraph.CI(");  
+  }
+  if(ipFactor) {
+    echo("\n\t\t\tx.factor=" + ipFactor);  
+  } else {}
+  if(ipResponse) {
+    echo(",\n\t\t\tresponse=" + ipResponse);  
+  } else {}
+  if(ipGroups) {
+    echo(",\n\t\t\tgroup=" + ipGroups);  
+  } else {}
+  if(ipPlotType == "line") {
+    if(ipPlotElements != "b") {
+      echo(",\n\t\t\ttype=\"" + ipPlotElements + "\"");  
+    } else {}  
+    if(!ipLegendChecked && ipGroups != "") {
+      echo(",\n\t\t\tlegend=FALSE");  
+    } else {}  
+    if(!ipDrawSEChecked) {
+      echo(",\n\t\t\tci.fun=function(x)c(mean(x, na.rm=TRUE), mean(x, na.rm=TRUE))");  
+    } else {}  
+  } else {
+    if(ipPlotBars == "split") {
+      echo(",\n\t\t\tsplit=TRUE");  
+    } else {}  
+    if(ipLegendChecked && ipGroups != "") {
+      echo(",\n\t\t\tlegend=TRUE");  
+    } else {}  
+    if(!ipDrawSEChecked) {
+      echo(",\n\t\t\tuc=FALSE,\n\t\t\tlc=FALSE");  
+    } else {}  
+    if(ipDrawSEChecked && !ipUpperError) {
+      echo(",\n\t\t\tuc=FALSE");  
+    } else {}  
+    if(ipDrawSEChecked && !ipLowerError) {
+      echo(",\n\t\t\tlc=FALSE");  
+    } else {}  
+    if(ipClipping == "figure") {
+      echo(",\n\t\t\txpd=TRUE");  
+    } else if(ipClipping == "device") {
+      echo(",\n\t\t\txpd=NA");  
+    } else {}  
+  }
+  if(ipLegendChecked && ipGroups != "" && ipLegendLabel != "") {
+    echo(",\n\t\t\tleg.lab=\"" + ipLegendLabel + "\"");  
+  } else {}
+  echo(embRkwrdpltptnGCodePrintout.replace(/, /g, ",\n\t\t\t"));
+  echo("\n\t\t)");
 
-	// insert any option-setting code that should be run before the actual plotting commands:
-	printIndentedUnlessEmpty("\t\t", embRkwrdpltptnGCodePreprocess, "\n", "");
+  // insert any option-setting code that should be run after the actual plot:
+  printIndentedUnlessEmpty("    ", embRkwrdpltptnGCodeCalculate, "\n", "");
 
-	// the actual plot:
-	var frmLegendChecked = getValue("frm_Legend.checked");
-	var frmDrwstndrChecked = getValue("frm_Drwstndr.checked");
-	if(radPlottype == "line") {
-		echo("\t\tlineplot.CI(");
-	} else {
-		echo("\t\tbargraph.CI(");
-	}
-	if(vrslFactrxxs) {
-		echo("\n\t\t\tx.factor=" + vrslFactrxxs);
-	}
-	if(vrslRspnsvct) {
-		echo(",\n\t\t\tresponse=" + vrslRspnsvct);
-	}
-	if(vrslGrpngfct) {
-		echo(",\n\t\t\tgroup=" + vrslGrpngfct);
-	}
-	if(radPlottype == "line") {
-		if(radElements != "b") {
-		echo(",\n\t\t\ttype=\"" + radElements + "\"");
-	}
-	if(!frmLegendChecked & vrslGrpngfct != "") {
-		echo(",\n\t\t\tlegend=FALSE");
-	}
-	if(!frmDrwstndrChecked) {
-		echo(",\n\t\t\tci.fun=function(x)c(mean(x, na.rm=TRUE), mean(x, na.rm=TRUE))");
-	}
-	} else {
-		if(radBars == "split") {
-		echo(",\n\t\t\tsplit=TRUE");
-	}
-	if(frmLegendChecked & vrslGrpngfct != "") {
-		echo(",\n\t\t\tlegend=TRUE");
-	}
-	if(!frmDrwstndrChecked) {
-		echo(",\n\t\t\tuc=FALSE,\n\t\t\tlc=FALSE");
-	}
-	if(frmDrwstndrChecked & !chcUpperrrr) {
-		echo(",\n\t\t\tuc=FALSE");
-	}
-	if(frmDrwstndrChecked & !chcLowerrrr) {
-		echo(",\n\t\t\tlc=FALSE");
-	}
-	if(drpClipping == "figure") {
-		echo(",\n\t\t\txpd=TRUE");
-	} else if(drpClipping == "device") {
-		echo(",\n\t\t\txpd=NA");
-	}
-	}
-	if(frmLegendChecked & vrslGrpngfct != "" & inpLegndlbl != "") {
-		echo(",\n\t\t\tleg.lab=\"" + inpLegndlbl + "\"");
-	}
-	echo(embRkwrdpltptnGCodePrintout.replace(/, /g, ",\n\t\t\t"));
-	echo("\n\t\t)");
-
-	// insert any option-setting code that should be run after the actual plot:
-	printIndentedUnlessEmpty("\t\t", embRkwrdpltptnGCodeCalculate, "\n", "");
-
-	echo("\n\t})\n");
-	if(full) {
-		echo("rk.graph.off()\n");
-	}
+  echo("\n  })\n");
+  if(full) {
+    echo("rk.graph.off()\n");
+  } else {}
 }
--- inst/rkward/plugins/Preparewithinsubjectdata.js
+++ inst/rkward/plugins/Preparewithinsubjectdata.js
@@ -4,83 +4,81 @@
 
 
 function preprocess(){
-	// add requirements etc. here
+  // add requirements etc. here
 
 }
 
 function calculate(){
-	// read in variables from dialog
+  // read in variables from dialog
+  var pdDataSelected = getString("pdDataSelected");
+  var pdResponse = getString("pdResponse");
+  var pdNameDependend = getString("pdNameDependend");
+  var pdNameCondition = getString("pdNameCondition");
+  var pdNameCaseID = getString("pdNameCaseID");
+  var pdCaseID = getString("pdCaseID");
+  var pdBetween = getString("pdBetween");
+  var pdSaveResults = getString("pdSaveResults");
+  var pdGenCaseID = getBoolean("pdGenCaseID.state");
 
-	var vrslSlctllvr = getString("vrsl_Slctllvr");
-	var vrslDpndntrs = getString("vrsl_Dpndntrs");
-	var inpNmfrdpnd = getString("inp_Nmfrdpnd");
-	var inpNmfrxprm = getString("inp_Nmfrxprm");
-	var inpNmfrcssb = getString("inp_Nmfrcssb");
-	var vrslCssbjctd = getString("vrsl_Cssbjctd");
-	var vrslBtwnsbjc = getString("vrsl_Btwnsbjc");
-	var svbSvrsltst = getString("svb_Svrsltst");
-	var chcAtmtccss = getBoolean("chc_Atmtccss.state");
-
-	// the R code to be evaluated
-	var vrslDpndntrsShortname = getValue("vrsl_Dpndntrs.shortname").split("\n").join("\", \"");
-	var vrslDpndntrs = getValue("vrsl_Dpndntrs").split("\n").join(",\n\t\t\t");
-	var vrslCssbjctdShortname = getValue("vrsl_Cssbjctd.shortname");
-	var vrslBtwnsbjcShortname = getValue("vrsl_Btwnsbjc.shortname");
-	var lngVrslBtwnsbjc = getValue("vrsl_Btwnsbjc").split("\n").join(",\n\t\t\t");
-	if(vrslSlctllvr) {
-		echo("\tnum.cases <- nrow(" + vrslSlctllvr + ")\n");
-	} else {
-		echo("\tnum.cases <- unique(sapply(list(\n\t\t\t" + vrslDpndntrs);
-		if(!chcAtmtccss && vrslCssbjctd) {
-			echo(",\n\t\t\t" + vrslCssbjctd);
-		}
-		if(vrslBtwnsbjc) {
-			echo(",\n\t\t\t" + lngVrslBtwnsbjc);
-		}
-		echo("),\n\t\tlength))\n\tif(length(num.cases) > 1) {" + "\n\t\tstop(simpleError(\"Can't determine number of cases, variables don't have equal length!\"))" + "\n\t}\n");
-	}
-	if(vrslDpndntrs) {
-		echo("\tanova.conditions <- c(\"" + vrslDpndntrsShortname + "\")\n\tnum.conditions <- length(anova.conditions)\n\n");
-	} else {}
-	if(vrslBtwnsbjc) {
-		var betweenVarsNames = vrslBtwnsbjcShortname.split("\n");
-	var betweenVars = vrslBtwnsbjc.split("\n");
-	} else {
-		var betweenVars = "";
-	}
-	echo("\tanova.data <- data.frame(");
-	if(vrslDpndntrs) {
-		echo("\n\t\t" + inpNmfrdpnd + "=c(\n\t\t\t" + vrslDpndntrs + ")" + ",\n\t\t" + inpNmfrxprm + "=factor(rep(anova.conditions, each=num.cases))");
-	} else {}
-	if(chcAtmtccss && inpNmfrcssb) {
-		echo(",\n\t\t" + inpNmfrcssb + "=factor(rep(1:num.cases, times=num.conditions))");
-	} else {}
-	if(!chcAtmtccss && vrslCssbjctd) {
-		echo(",\n\t\t" + vrslCssbjctdShortname + "=factor(rep(" + vrslCssbjctd + ", times=num.conditions))");
-	} else {}
-	if(vrslBtwnsbjc) {
-		for (var i=0, len=betweenVarsNames.length; i<len; ++i ){
-			echo(",\n\t\t" + betweenVarsNames[i] + "=factor(rep(" + betweenVars[i] + ", times=num.conditions))");
-		}
-	} else {}
-	echo(",\n\t\tstringsAsFactors=FALSE)\n\n");
+  // the R code to be evaluated
+  var pdResponseShortname = getValue("pdResponse.shortname").split("\n").join("\", \"");
+  var pdResponse = getValue("pdResponse").split("\n").join(",\n\t\t\t");
+  var pdCaseIDShortname = getValue("pdCaseID.shortname");
+  var pdBetweenShortname = getValue("pdBetween.shortname");
+  var lngPdBetween = getValue("pdBetween").split("\n").join(",\n\t\t\t");
+  if(pdDataSelected) {
+    echo("\tnum.cases <- nrow(" + pdDataSelected + ")\n");  
+  } else {
+    echo("\tnum.cases <- unique(sapply(list(\n\t\t\t" + pdResponse);  
+    if(!pdGenCaseID && pdCaseID) {
+      echo(",\n\t\t\t" + pdCaseID);  
+    } else {}  
+    if(pdBetween) {
+      echo(",\n\t\t\t" + lngPdBetween);  
+    } else {}  
+    echo("),\n\t\tlength))\n\tif(length(num.cases) > 1) {" + "\n\t\tstop(simpleError(" + i18n("Can't determine number of cases, variables don't have equal length!") + "))" + "\n\t}\n");  
+  }
+  if(pdResponse) {
+    echo("\tanova.conditions <- c(\"" + pdResponseShortname + "\")\n\tnum.conditions <- length(anova.conditions)\n\n");  
+  } else {}
+  if(pdBetween) {
+    var betweenVarsNames = pdBetweenShortname.split("\n");  
+    var betweenVars = pdBetween.split("\n");  
+  } else {
+    var betweenVars = "";  
+  }
+  echo("\tanova.data <- data.frame(");
+  if(pdResponse) {
+    echo("\n\t\t" + pdNameDependend + "=c(\n\t\t\t" + pdResponse + ")" + ",\n\t\t" + pdNameCondition + "=factor(rep(anova.conditions, each=num.cases))");  
+  } else {}
+  if(pdGenCaseID && pdNameCaseID) {
+    echo(",\n\t\t" + pdNameCaseID + "=factor(rep(1:num.cases, times=num.conditions))");  
+  } else {}
+  if(!pdGenCaseID && pdCaseID) {
+    echo(",\n\t\t" + pdCaseIDShortname + "=factor(rep(" + pdCaseID + ", times=num.conditions))");  
+  } else {}
+  if(pdBetween) {
+    for (var i=0, len=betweenVarsNames.length; i<len; ++i ){
+      echo(",\n\t\t" + betweenVarsNames[i] + "=factor(rep(" + betweenVars[i] + ", times=num.conditions))");
+    }  
+  } else {}
+  echo(",\n\t\tstringsAsFactors=FALSE)\n\n");
 }
 
 function printout(){
-	// printout the results
-	new Header(i18n("Prepare within subject data")).print();
+  // printout the results
+  new Header(i18n("Prepare within subject data")).print();
 
-
-	echo("\trk.print(summary(anova.data))\n");
-	//// save result object
-	// read in saveobject variables
-	var svbSvrsltst = getValue("svb_Svrsltst");
-	var svbSvrsltstActive = getValue("svb_Svrsltst.active");
-	var svbSvrsltstParent = getValue("svb_Svrsltst.parent");
-	// assign object to chosen environment
-	if(svbSvrsltstActive) {
-		echo(".GlobalEnv$" + svbSvrsltst + " <- anova.data\n");
-	}
+  echo("\trk.print(summary(anova.data))\n");
+  //// save result object
+  // read in saveobject variables
+  var pdSaveResults = getValue("pdSaveResults");
+  var pdSaveResultsActive = getValue("pdSaveResults.active");
+  var pdSaveResultsParent = getValue("pdSaveResults.parent");
+  // assign object to chosen environment
+  if(pdSaveResultsActive) {
+    echo(".GlobalEnv$" + pdSaveResults + " <- anova.data\n");
+  } else {}
 
 }
 
